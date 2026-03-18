03e8b037728689e0a890c1421de1adfe16e0abb8
lee <codingnet_school@naver.com>
2026-03-18 12:02:24 +0900

    2026-03-18 commit

---
+++ javascript/hello.js
@@ -0,0 +1,10 @@
+/*
+    hello.js -> 자바스크립트 엔진(Node.js)에서 바로 실행
+*/
+let a = 100;
+let b = 200;
+let sum = a + b;
+console.log('a = ', a);
+console.log('b = ', b);
+console.log('sum = ', sum);
+console.log('a + b = ', (a + b));
