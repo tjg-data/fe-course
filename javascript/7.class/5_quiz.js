71662ff54dcea9e6edf5a85f3fd95708058da934
lee <codingnet_school@naver.com>
2026-03-25 14:12:44 +0900

    commit

--- javascript/7.class/5_quiz.js
+++ javascript/7.class/5_quiz.js
@@ -11,8 +11,55 @@ let dataList = [
     { name: '홍길순', age: 24, address: '서울시', emoji: '👩'},
 ];
 
-/** 학생(Student) 클래스 정의 */
+/** 1. 학생(Student) 클래스 정의 */
 class Student {
+    #name;
+    #age;
+    #address;
 
+    constructor(name, age, address, emoji) {
+        this.#name = name;
+        this.#age = age;
+        this.#address = address;
+        this.emoji = emoji;
+    }
 
+    getName = () => this.#name;
+    getAge = () => this.#age;
+    getAddress = () => this.#address;
+    getEmoji = () => this.emoji;
+
+    setName = (name) => this.#name = name;
+    setAge = (age)   => this.#age = age;
+    setAddress = (address) => this.#address = address;
+    setEmoji = (emoji) => this.emoji = emoji;
+
+    display = () => console.log(this.#name, this.#age, this.#address, this.emoji);
 }
+// { name: '홍길동', age: 20, address: '서울시', emoji: '🧑'},
+
+/** 2. 학생(Student) 객체 생성 */
+let hong = new Student('홍길동', 20, '서울시', '🧑');
+hong.display();
+
+/** 3. dataList 배열 객체를 이용하여 학생(Student) 객체 생성 후 
+ *      학생 리스트(배열)에 저장 */
+const studentList = [];
+for(let i=0; i<dataList.length; i++) {
+    let obj = dataList[i]; 
+    let student = new Student(obj.name, obj.age, obj.address, obj.emoji);
+    studentList.push(student);
+}
+console.log(studentList);
+
+/** 4. studentList 배열의 객체 출력 */
+console.log(`------- 학생 리스트 -------`);
+for(let i=0; i<studentList.length; i++) {
+    let student = studentList[i];
+    console.log(student.getName()+'\t',
+    student.getAge() +'\t',
+    student.getAddress() + '\t',
+    student.getEmoji() );    
+}
+console.log(`---------------------------`);
+
