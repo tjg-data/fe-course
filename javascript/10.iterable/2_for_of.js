ee08371e97a520c0e149d99ee775cf6af44d6f47
lee <codingnet_school@naver.com>
2026-03-27 14:29:47 +0900

    2026-03-27 commit

--- javascript/10.iterable/2_for_of.js
+++ javascript/10.iterable/2_for_of.js
@@ -3,11 +3,72 @@
  * - for(const(let) 변수 of 순회객체) { 실행문; }
  */
 
-/* 배열의 요소를 치환하는 함수 생성 */
-function replace() {
+/* 배열의 요소를 치환하는 함수 생성 : for..of */
+function replace(array, oldItem, newItem) {
+    //array(= fruits)의 복사본 객체, shallow copy
+    let arrayCopy = Array.from(array);
 
+    let idx = 0;
+    for(const item of arrayCopy) {        
+        if(item === oldItem) { 
+            arrayCopy.splice(idx, 1, newItem); //배열객체.splice(item idx, count, newItem)     
+        }
+        idx++;
+    }
+    return arrayCopy;
+}
+
+//forEach
+function replace1(array, oldItem, newItem) {  
+    //array(= fruits)의 복사본 객체, shallow copy
+    let arrayCopy = Array.from(array);
+
+    arrayCopy.forEach((item, idx) => {
+        if(item === oldItem) arrayCopy.splice(idx, 1, newItem);
+    });
+    
+    return arrayCopy;
+}
+
+//map
+function replace2(array, oldItem, newItem) {  
+    //array(= fruits)의 복사본 객체, shallow copy
+    let arrayCopy = Array.from(array);
+
+    arrayCopy.forEach((item, idx) => {
+        if(item === oldItem) arrayCopy.splice(idx, 1, newItem);
+    });
+    
+    return arrayCopy;
+}
+
+//replaceObj
+function replaceObj(array, oldItem, newItem) {
+    //array(= fruits)의 복사본 객체, shallow copy
+    let arrayCopy = Array.from(array); 
+
+    let idx = 0;
+    for(const item of arrayCopy) {        
+        if(item.emoji === oldItem) { 
+            arrayCopy.splice(idx, 1, {name:item.name, emoji:newItem}); 
+        }
+        idx++;
+    }
+    return arrayCopy;
 }
 
 let fruits = ['🍊','🍋','🍎','🍓', '🍏'];
+let fruitsObj = [
+    {name: 'orange', emoji: '🍊'},
+    {name: 'lemon', emoji: '🍋'},
+];
+let obj1 = replace(fruits, '🍎', '🍏');  //for..of
+let obj1_1 = replace1(fruits, '🍎', '🍏');  //forEach
+let obj2 = replaceObj(fruitsObj, '🍋', '🍏');
 
-let obj1 = replace(fruits, '🍎', '🍏');
+console.log(fruits);
+console.log(obj1);
+console.log(obj1_1);
+// console.log();
+// console.log(fruitsObj);
+// console.log(obj2);
