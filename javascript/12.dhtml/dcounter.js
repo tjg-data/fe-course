0600803c16072dfe121e4c4473090dfdcadc5e2f
lee <codingnet_school@naver.com>
2026-03-30 18:19:03 +0900

    2026-03-30 commit

--- javascript/12.dhtml/dcounter.js
+++ javascript/12.dhtml/dcounter.js
@@ -12,9 +12,36 @@ function initForm() {
         <h1>Counter</h1>
         <div>
             <h2 id="number">0</h2>
-            <button type="button" onclick="counter('+')">increment(+)</button>
-            <button type="button" onclick="counter('-')">decrement(-)</button>
+            <!--
+                <button type="button" onclick="counter('+')">increment(+)</button>
+                <button type="button" onclick="counter('-')">decrement(-)</button>
+            -->
+            <button type="button" class="button" data-operation="increment">increment(+)</button>
+            <button type="button" class="button" data-operation="decrement">decrement(-)</button>
         </div>
     `;
+    //✨ DHTML에서는 화면을 구성한 후 반드시 이벤트 처리를 해야함!!
     document.querySelector('#content').innerHTML = output;
+
+    //버튼 이벤트 관리
+    let buttons = document.querySelectorAll('.button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let number = document.querySelector('#number').textContent;
+            let type = button.dataset.operation;
+            if(type === 'increment') {
+                if(number >= 0) {
+                    document.querySelector('#number').textContent = ++number;
+                }
+            } else {
+                if(number > 0) {
+                    document.querySelector('#number').textContent = --number;
+                }
+            }   
+        });
+    });
 }
+
+
+
