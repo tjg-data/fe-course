5e840d2d3086c2dbc08cd0a9f0023a180b851065
lee <codingnet_school@naver.com>
2026-04-03 15:28:07 +0900

    2026-04-03 commit~

--- cgv-demo/js/cgv_support.js
+++ cgv-demo/js/cgv_support.js
@@ -15,4 +15,45 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    /** 테이블 생성 함수 */
+    createTable();
+
 }); //window event
+
+
+//support.json JSON 데이터 가져오기
+async function getJson() {
+    let response = await fetch("../data/support.json");
+    return response.json();
+}
+
+async function createTable() {
+    let list = await getJson();
+    // console.log('list =>', list);
+    let output = `
+        <table>
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>구분</th>
+                    <th>제목</th>
+                    <th>등록일</th>
+                    <th>조회수</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${
+
+
+            }
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5">1 2 3 4 5  >></td>
+                </tr>
+            </tfoot>
+        </table>
+    `;
+
+    
+}
