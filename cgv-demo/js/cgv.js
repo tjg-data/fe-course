c60dd62ff32a7c4440c4ff34072888e4de94be65
lee <codingnet_school@naver.com>
2026-04-03 12:19:20 +0900

    2026-04-03 commit~

--- cgv-demo/js/cgv.js
+++ cgv-demo/js/cgv.js
@@ -1,39 +1,69 @@
+/**
+ * DOM 객체 생성 후 객체별 이벤트 호출
+ */
 window.addEventListener('DOMContentLoaded', () => {
+
     let id = document.querySelector('#id');
     let pwd = document.querySelector('#pwd');
     let idMsg = document.querySelector('#idMsg');
     let pwdMsg = document.querySelector('#pwdMsg');
+    let btnLogin = document.querySelector('#btnLogin');
 
     id.addEventListener('input', () => {
         if(id.value.trim() !== '') {
             idMsg.style.display = 'none';
         }
     });
+    pwd.addEventListener('input', () => {
+        if(pwd.value.trim() !== '') {
+            pwdMsg.style.display = 'none';
+        }
+    });
+    btnLogin.addEventListener('click', () => {
+        let isValid = true;
+
+        if(id.value.trim() === '') {
+            idMsg.style.display = 'block';
+            idMsg.style.fontSize = '12px';
+            idMsg.style.color = 'red';
+            idMsg.textContent = '아이디를 입력해주세요';
+            id.focus();
+            isValid = false;
+        } else if(pwd.value.trim() === '') {
+            pwdMsg.style.display = 'block';
+            pwdMsg.style.fontSize = '12px';
+            pwdMsg.style.color = 'red';
+            pwdMsg.textContent = '비밀번호를 입력해주세요';
+            pwd.focus();
+            isValid = false;
+        } else {
+            if(isValid) {
+                console.log('서버전송--->', id.value, pwd.value);
+            }
+        }
+    });
+
+
 });
 
 
 /**
  * loginCheck() - 로그인 유효성 체크 함수 
  */
+/*
 const loginCheck = () => {
     let id = document.querySelector('#id');
-    let pwd = document.querySelector('#pwd');
-    let idMsg = document.querySelector('#idMsg');
-    let pwdMsg = document.querySelector('#pwdMsg');
+    let pwd = document.querySelector('#pwd');    
     // console.log(id, pwd);
     if(id.value === '') {
-        idMsg.textContent = '아이디를 입력해주세요';
-        idMsg.style.fontSize = '12px';
-        idMsg.style.color = 'red'; 
+        alert('아이디를 입력해주세요');
         id.focus();
     } else if(pwd.value === '') {
-        pwdMsg.textContent = '비밀번호를 입력해주세요';
-        pwdMsg.style.fontSize = '12px';
-        pwdMsg.style.color = 'red'; 
+        alert('비밀번호를 입력해주세요');
         pwd.focus();
     } else {
         //서버 전송!!
         console.log('서버전송---> ', id.value, pwd.value);
-    }   
-    
+    }       
 }
+*/
