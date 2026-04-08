dd0c9325294e40faa492279fb522d9c47a0b83b5
lee <codingnet_school@naver.com>
2026-04-08 16:06:10 +0900

    2026-04-08 commit~

--- basic-app/src/App.jsx
+++ basic-app/src/App.jsx
@@ -2,28 +2,29 @@ import AvatarImage from "./components/avatar/AvatarImage.jsx"
 import AvatarImageList from "./components/avatar/AvatarImageList.jsx"
 import Avatar from "./components/avatar/Avatar.jsx"
 import AvatarList from "./components/avatar/AvatarList.jsx"
+import style from './components/avatar/Avatar.module.css';
 import Menu from './components/menu/Menu.jsx';
-import style from './components/menu/Menu.module.css';
+import mstyle from './components/menu/Menu.module.css';
 import people1 from './assets/people1.webp';
 import people2 from './assets/people2.webp';
 import people3 from './assets/people3.webp';
 
 export default function App() {
   const list = [
-    {img: people1},
-    {img: people2},
-    {img: people3},
-    {img: people3}
+    {img: people1, style:style.avatar_img},
+    {img: people2, style:style.avatar_img},
+    {img: people3, style:style.avatar_img_circle},
+    {img: people3, style:style.avatar_img}
   ]
   const alist = [
-    {img: people1, name: "Smith"},
+    {img: people1, name: "Smith" },
     {img: people2, name: "James"},
     {img: people3, name: "Anne"},
     {img: people3, name: "Anne"}
   ]
   return ( 
     <>
-      <AvatarImage img={people1} />
+      <AvatarImage img={people1} style={style.avatar_img}/>
       <Avatar name="Anne" img={people3} />
       <AvatarImageList imgList={list} />
       <AvatarList list={alist} />
