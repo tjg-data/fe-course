08c18cb39f2ce216bce32f5b85bdf0cdc799df6a
lee <codingnet_school@naver.com>
2026-04-08 15:26:14 +0900

    2026-04-08 commit~

--- basic-app/src/App.jsx
+++ basic-app/src/App.jsx
@@ -1,5 +1,7 @@
 import AvatarImage from "./components/avatar/AvatarImage.jsx"
+import AvatarImageList from "./components/avatar/AvatarImageList.jsx"
 import Avatar from "./components/avatar/Avatar.jsx"
+import AvatarList from "./components/avatar/AvatarList.jsx"
 import Menu from './components/menu/Menu.jsx';
 import style from './components/menu/Menu.module.css';
 import people1 from './assets/people1.webp';
@@ -7,11 +9,24 @@ import people2 from './assets/people2.webp';
 import people3 from './assets/people3.webp';
 
 export default function App() {
-
+  const list = [
+    {img: people1},
+    {img: people2},
+    {img: people3},
+    {img: people3}
+  ]
+  const alist = [
+    {img: people1, name: "Smith"},
+    {img: people2, name: "James"},
+    {img: people3, name: "Anne"},
+    {img: people3, name: "Anne"}
+  ]
   return ( 
     <>
       <AvatarImage img={people1} />
       <Avatar name="Anne" img={people3} />
+      <AvatarImageList imgList={list} />
+      <AvatarList list={alist} />
     </>
   )
 }
--- basic-app/src/components/avatar/Avatar.jsx
+++ basic-app/src/components/avatar/Avatar.jsx
@@ -2,10 +2,11 @@
  * Avatar.jsx - 아바타이미지 + 이름
  */
 import AvatarImage from "./AvatarImage.jsx"
+import style from './Avatar.module.css';
 
 export default function Avatar({name, img}) {
     return (
-        <div>
+        <div className={style.avatar}>
             <AvatarImage img={img}/>
             <p>{name}</p>
         </div>
