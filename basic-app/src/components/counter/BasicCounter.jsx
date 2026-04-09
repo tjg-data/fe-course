97fa732c8914b93a64b7d7f1490c3f9ac46ace08
lee <codingnet_school@naver.com>
2026-04-09 15:04:11 +0900

    2026-04-09 commit~

--- basic-app/src/components/counter/BasicCounter.jsx
+++ basic-app/src/components/counter/BasicCounter.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react';
 
 export default function BasicCounter() {
     let [count, setCount] = useState(0);
+    let [flag, setFlag] = useState(false);
 
     const handleCounter = (e) => {
         let type = e.target.name;
         if(type === "+") {
-            if(count < 10) setCount(count+1); 
+            if(count < 10) {
+                setCount(count+1);  
+                setFlag(true);           
+            }
         } else if(type === "-") {
-            if(count > 0) setCount(count-1);
+            if(count > 0) {
+                setCount(count-1);
+                setFlag(false);
+            }
         } else {
             setCount(0);
         }
@@ -16,7 +23,12 @@ export default function BasicCounter() {
     return (
         <>
             <h1>React Counter</h1>
-            <h2>Counter :: <span>{count}</span></h2>
+            <h2>Counter :: 
+                { 
+                    flag? <span style={{color:'red'}}>{count}</span> 
+                            : <span style={{color:'blue'}}>{count}</span>
+                }  
+            </h2>
             <div>
                 <button type="button" name="+" onClick={handleCounter}>증가(+)</button>
                 <button type="button" name="-" onClick={handleCounter}>감소(-)</button>
