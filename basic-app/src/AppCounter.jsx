d2cd4abc95c22bdec26184cfc38be8886c263b24
lee <codingnet_school@naver.com>
2026-04-09 16:04:06 +0900

    2026-04-09 commit~

--- basic-app/src/AppCounter.jsx
+++ basic-app/src/AppCounter.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import BasicCounter from './components/counter/BasicCounter.jsx';
 
 export default function App() {
+    // let totalCount = 0;  //값이 변경되면 자동 리렌더링❌
+    const [totalCount, setTotalCount] = useState(0);  //값이 변경되면 자동 리렌더링⭕
+    const hadleTotalCount = (type) => {
+        if(type === "+") setTotalCount(totalCount + 1);
+        else setTotalCount(totalCount - 1);
+    }
     return (
         <>
-            <BasicCounter />
+            <h1>AppCounter</h1>
+            <h1>Total Count :: <span><b>{totalCount}</b></span></h1>
+            <BasicCounter click={hadleTotalCount} total={totalCount}/>
+            <BasicCounter click={hadleTotalCount} total={totalCount}/>
+            <BasicCounter click={hadleTotalCount} total={totalCount}/>
         </>
     )
 }
--- basic-app/src/components/counter/BasicCounter.jsx
+++ basic-app/src/components/counter/BasicCounter.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function BasicCounter() {
+export default function BasicCounter({ click, total }) {
     let [count, setCount] = useState(0);
     let [flag, setFlag] = useState(false);
 
@@ -9,24 +9,28 @@ export default function BasicCounter() {
         if(type === "+") {
             if(count < 10) {
                 setCount(count+1);  
-                setFlag(true);           
+                setFlag(true);
+                click(type);           
             }
         } else if(type === "-") {
             if(count > 0) {
                 setCount(count-1);
                 setFlag(false);
+                click(type);
             }
         } else {
             setCount(0);
         }
     }
     return (
-        <>
+        <div style={{   border: '1px solid gray', 
+                        padding: '10px 20px',
+                        margin: '20px'}}>
             <h1>React Counter</h1>
             <h2>Counter :: 
                 { 
-                    flag? <span style={{color:'red'}}>{count}</span> 
-                            : <span style={{color:'blue'}}>{count}</span>
+                    flag? <span style={{color:'red'}}>{count} / {total}</span> 
+                            : <span style={{color:'blue'}}>{count} / {total}</span>
                 }  
             </h2>
             <div>
@@ -34,6 +38,6 @@ export default function BasicCounter() {
                 <button type="button" name="-" onClick={handleCounter}>감소(-)</button>
                 <button type="button" name="reset" onClick={handleCounter}>초기화</button>
             </div>
-        </>
+        </div>
     )
 }
