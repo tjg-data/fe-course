caeff99911d7b9061dab1e9d4265982ddf02d8a1
lee <codingnet_school@naver.com>
2026-04-13 12:29:27 +0900

    2026-04-13 commit~

--- basic-app/src/components/form/TextInput.jsx
+++ basic-app/src/components/form/TextInput.jsx
@@ -1,34 +1,13 @@
-import { useRef, useState } from 'react';
 
-export default function TextInput() {
-    const textRef = useRef(null);
-    const [text, setText] = useState('');
-
-    const handleClick = () => {
-        let text = textRef.current.value.trim();
-        if(text === "") {
-            alert("텍스트를 입력해주세요");
-            textRef.current.focus();
-        } else {
-            alert("서버 전송-->" + text);
-        }
-    }
-    const handleChange = (e) => {
-        setText(e.target.value); 
-    }
-    console.log('text-->', text);        
-
+export default function TextInput({ item, value, handleChange }) {
+    const { type, name } = item;
     return (
-        <div>
-            <h2>Ref : 텍스트폼 입력 </h2>
-            <input  type="text" 
-                    name="text"
-                    value={text}
-                    ref={textRef}
-                    onChange={handleChange}
-                    />
-            <button type="button"
-                    onClick={handleClick}>전송</button>
-        </div>
+        <input  type={type}
+                id={name} 
+                name={name}
+                value={value}
+                onChange={(e)=>{ handleChange(e)}}
+                // ref={refs.addressRef}
+            />
     )
 }
