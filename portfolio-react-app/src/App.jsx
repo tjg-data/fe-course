da985ac6868932051cceff3474b1bdd66a7e2416
lee <codingnet_school@naver.com>
2026-04-14 17:16:08 +0900

    2026-04-14 commit~

--- portfolio-react-app/src/App.jsx
+++ portfolio-react-app/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { fetchData } from './util/fetch.js';
 import Header from './components/Header.jsx';
 import Content from './components/Content.jsx';
 import Footer from './components/Footer.jsx';
 
 export default function App() {
+  const [data, setData] = useState({});
+  useEffect(()=>{
+    const loadData = async() => {
+      const jsonData = await fetchData("http://localhost:5173/data/portfolio.json");
+      setData(jsonData);
+    }
+    loadData();
+  }, []);
 
   return (
     <>
-      <Header />
+      <Header data={data?.header} />
       <Content />
       <Footer />
     </>
--- portfolio-react-app/src/components/Header.jsx
+++ portfolio-react-app/src/components/Header.jsx
@@ -1,15 +1,12 @@
 import Logo from "./header/Logo.jsx";
+import ToggleButton from "./header/ToggleButton.jsx";
 import MenuList from "./commons/MenuList.jsx";
 
-export default function Header() {
-    const menus = [
-        {"href": "#home",   "style": "header-menu-item active", "name": "Home"},
-        {"href": "#about",  "style": "header-menu-item", "name": "About"},
-        {"href": "#skills", "style": "header-menu-item", "name": "Skills"},
-        {"href": "#work",   "style": "header-menu-item", "name": "My Work"},
-        {"href": "#testimonial", "style": "header-menu-item", "name": "Testimonial"},
-        {"href": "#contact",     "style": "header-menu-item", "name": "Contact"}
-    ]
+export default function Header({ data }) {
+    //✨data 객체가 존재하는 경우에만 구조분해할당 진행! 
+    //✨구조분해 할당으로 반환하는 타입이 객체인 경우에는 초기화 작업 필수
+    const { menus = [] } = data || {};  
+
     return (
         <header class="header">
             <Logo   img="/images/favicon.ico"
@@ -18,7 +15,7 @@ export default function Header() {
                     title="Judy"
             />
             <MenuList menus={menus} style="header-menu open" />
-            {/* <button class="header-toggle" id="menu-toggle"><i class="fa-solid fa-bars"></i></button> */}
+            <ToggleButton />
         </header>
     )
 }
