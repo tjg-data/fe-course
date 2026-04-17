f5d1531ab0075b588f42c13dd3c237459030db57
lee <codingnet_school@naver.com>
2026-04-17 10:47:52 +0900

    2026-04-17 commit

--- portfolio-react-spa/src/components/content/Modal.jsx
+++ portfolio-react-spa/src/components/content/Modal.jsx
@@ -12,6 +12,10 @@ export default function Modal({ project, onClose }) {
                 <h2>{project.title}</h2>
                 <img src={project.img} alt={project.alt} />
                 <p>{project.description}</p>
+                <h3>
+                    <button className="modal-like-button"
+                            onClick={()=> alert("좋아요 버튼 클릭!!")}>❤좋아요</button>
+                </h3>
             </div>
         </div>
     );
