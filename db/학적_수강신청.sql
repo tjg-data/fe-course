26639dec0fe80c3883f6e6a6a069cb4eaa4640ca
lee <codingnet_school@naver.com>
2026-04-28 16:51:54 +0900

    2026-04-28 commit~

---
+++ db/학적_수강신청.sql
@@ -0,0 +1,9 @@
+/********************************
+	학적과 수강신청 실습 데이터베이스
+********************************/
+-- 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS `enroll2026`;
+show databases;
+use enroll2026;
+select database();
+
