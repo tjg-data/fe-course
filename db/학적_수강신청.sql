0571e69c5b7273924da22b8dad8ab8d835beed3c
lee <codingnet_school@naver.com>
2026-04-29 10:32:54 +0900

    2026-04-29 commit

--- db/학적_수강신청.sql
+++ db/학적_수강신청.sql
@@ -2,8 +2,79 @@
 	학적과 수강신청 실습 데이터베이스
 ********************************/
 -- 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS `enroll2026`;
+-- CREATE DATABASE IF NOT EXISTS `enroll2026`;
 show databases;
 use enroll2026;
 select database();
+
+-- [instructor:강사] 테이블 생성
+show tables;
+create table instructor(
+	instructor_no 		int			auto_increment 		primary key,
+    instructor_name		varchar(5)	not null,
+    age					int,
+    gender				char(1)
+);
+desc instructor;
+
+-- [student : 학생] 테이블 생성
+create table student(
+	student_id		int			auto_increment		primary key,
+    student_name	varchar(5)	not null,
+    address			varchar(50)
+);
+desc student;
+
+-- [subject : 과목] 테이블 생성
+create table subject(
+	subject_no		int			auto_increment 		primary key,
+    subject_name	varchar(30)	not null,
+	class_room		varchar(20),
+    instructor_no	int			not null,
+    constraint fk_subject_instructor_no	foreign key(instructor_no)
+		references instructor(instructor_no)
+        on delete cascade
+        on update cascade
+);
+desc subject;
+select * from information_schema.table_constraints
+	where table_name = 'subject';
+
+-- [class_time : 강의시간] 테이블 생성
+create table class_time(
+	time_id		int			auto_increment		primary key,
+    class_time	varchar(10),
+    subject_no 	int			not null,
+    constraint fk_class_time_subject_no		foreign key(subject_no)
+		references	subject(subject_no)
+			on delete cascade
+            on update cascade
+);
+desc class_time;
+select * from information_schema.table_constraints
+	where table_name = 'class_time';
+    
+
+-- [enrollment : 등록] 테이블 생성
+create table enrollment(
+	student_id		int		not null,
+    subject_no		int		not null,
+    grade			char(1),
+    constraint fk_enrollment_student_id		foreign key(student_id)
+		references	student(student_id)
+			on delete cascade
+            on update cascade,
+	constraint fk_subject_subject_no		foreign key(subject_no)
+		references subject(subject_no)
+			on delete cascade
+            on update cascade
+);
+desc enrollment;
+select * from information_schema.table_constraints
+	where table_name = 'enrollment';
+
+
+
+
+
 
